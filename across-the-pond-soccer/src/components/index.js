db0c7e21062dc38ef73676ae89cc8eb2dcb7285b
ameyer12 <105730686+ameyer12@users.noreply.github.com>
2022-12-02 22:12:17 -0600

    Delete across-the-pond-soccer directory

--- across-the-pond-soccer/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Home } from "./Home"
-export {default as NavBar } from "./NavBar"
-export {default as TopScorersPage } from "./TopScorersPage"
-export {default as TopAssistersPage } from "./TopAssistersPage"
-
-

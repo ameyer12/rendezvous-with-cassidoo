75ae6c068651bf9e05b621a926f1fdb29b94ddb0
Austin <austinmeyer1106@gmail.com>
2022-12-27 18:40:22 -0600

    fixed typo

--- Portfolio/src/components/Project2.js
+++ Portfolio/src/components/Project2.js
@@ -6,7 +6,7 @@ function Project2() {
       <div id="project2-body" className="animate__animated animate__fadeInDown">
           <h4>Fitness Tracker</h4>
           <p id="project2-description">
-            A REACT app that allows users to register an account, build fitness routines, create custom workouts, and browse routines and activities created by other users. Tech stack: REACT, JSON, HTML, REST API, HTML/CSS, Bootstrap, Node, Express, and PostgreSQL.
+            A REACT app that allows users to register an account, build fitness routines, create custom workouts, and browse routines and activities created by other users. Tech stack: REACT, JSON, REST API, HTML/CSS, Bootstrap, Node, Express, and PostgreSQL.
           </p>
           <div>
             <ul id="project2-links-ul">

b74ad50068c60dd17fba2991fc0145b9b9a5502a
Austin <austinmeyer1106@gmail.com>
2023-01-24 11:39:28 -0600

    fixing mobile

--- Portfolio/src/components/About.js
+++ Portfolio/src/components/About.js
@@ -41,26 +41,6 @@ function About() {
             <img id="about-image" src="https://i.postimg.cc/J0XB42wS/IMG-0955.jpg" alt="Graduation Photo"></img>
           </div>
         </section>
-        {/* <section id='about-body'>
-          <h3 id="about-h3">About Me</h3>
-          <p className="about-me-paragraph">My name is Austin Meyer. I’m 23 years old, and I live in Fort Worth, Texas.
-            I attended The University of Texas at Austin and received my Bachelor of Arts in English
-            in 2021. Since then, I've been working full-time while attending Fullstack Academy
-            to learn web development and UI design. </p>
-            <img id="about-image" src="https://i.postimg.cc/J0XB42wS/IMG-0955.jpg" alt="Graduation Photo"></img>
-          <h3 className="about-skills-section">Skills</h3>
-          <p className="skills-bullets">
-            » JavaScript (ES6+) <span className="right-bullet">» React</span>
-            <br></br>
-            » Node.js <span className="right-bullet">» Express.js</span>
-            <br></br>
-            » HTML <span className="right-bullet">» CSS</span>
-            <br></br>
-            » Bootstrap <span className="right-bullet">» Figma</span>
-            <br></br>
-            » Git <span className="right-bullet">» GitHub</span>
-            </p>
-        </section> */}
       </div>
     )
   }

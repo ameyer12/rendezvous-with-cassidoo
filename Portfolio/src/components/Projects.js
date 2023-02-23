90be08c11d7cdbb4dd37ffe1974f900dfe09c516
Austin <austinmeyer1106@gmail.com>
2023-02-23 13:21:09 -0600

    fixed a typo, added a project card for a recently completed project, and added another card for an upcoming card

--- Portfolio/src/components/Projects.js
+++ Portfolio/src/components/Projects.js
@@ -7,7 +7,8 @@ import fitnessTrackerImage from "./Fitness Tracker.png";
 import strangersThingsImage from "./Stranger's Things.png";
 import guessingGameImage from "./Guessing Game.png";
 import toDoAppImage from "./To Do App.png";
-// import underConstructionImage from "./Under Construction.png";
+import informationalWebsiteImage from "./Informational Website.png";
+import underConstructionImage from "./Under Construction.png";
 import "animate.css";
 
 function Projects() {
@@ -18,7 +19,7 @@ function Projects() {
        <h3 id="projects-h3">Projects</h3>
         <div className="projects-container">
           <div class="card" id="project-card">
-            <img class="card-img-top" src={theClosetImage} alt="Project Image"></img>
+            <img class="card-img-top" src={theClosetImage} alt="Project"></img>
             <div class="card-body">
               <h5 class="card-title">The Closet</h5>
               <p class="card-text"> A full stack e-commerce clothing application.
@@ -31,7 +32,7 @@ function Projects() {
             </div>
           </div>
           <div class="card" id="project-card">
-            <img class="card-img-top" src={acrossThePondSoccerImage} alt="Project Image"></img>
+            <img class="card-img-top" src={acrossThePondSoccerImage} alt="Project"></img>
             <div class="card-body">
               <h5 class="card-title">Across The Pond Soccer</h5>
               <p class="card-text"> A web application for users to view the top scorers and assisters in Europe's top 5 soccer leagues.
@@ -44,7 +45,7 @@ function Projects() {
             </div>
           </div>
           <div class="card" id="project-card">
-            <img class="card-img-top" src={fitnessTrackerImage} alt="Project Image"></img>
+            <img class="card-img-top" src={fitnessTrackerImage} alt="Project"></img>
             <div class="card-body">
               <h5 class="card-title">Fitness Tracker</h5>
               <p class="card-text"> A web app for users to build fitness routines and create custom workouts.
@@ -57,7 +58,7 @@ function Projects() {
             </div>
           </div>
           <div class="card" id="project-card">
-            <img class="card-img-top" src={strangersThingsImage} alt="Project Image"></img>
+            <img class="card-img-top" src={strangersThingsImage} alt="Project"></img>
             <div class="card-body">
               <h5 class="card-title">Stranger's Things</h5>
               <p class="card-text"> A React CRUD app that allows users to register an account and buy and sell items on the marketplace.
@@ -70,7 +71,7 @@ function Projects() {
             </div>
           </div>
           <div class="card" id="project-card">
-            <img class="card-img-top" src={guessingGameImage} alt="Project Image"></img>
+            <img class="card-img-top" src={guessingGameImage} alt="Project"></img>
             <div class="card-body">
               <h5 class="card-title">Guessing Game</h5>
               <p class="card-text"> A guessing game web app. Users have 5 guesses to guess a random number between 1 and 100.
@@ -84,17 +85,69 @@ function Projects() {
             </div>
           </div>
           <div class="card" id="project-card">
-            <img class="card-img-top" src={toDoAppImage} alt="Project Image"></img>
+            <img class="card-img-top" src={toDoAppImage} alt="Project"></img>
             <div class="card-body">
               <h5 class="card-title">To-Do List</h5>
-              <p class="card-text">A web app that allows users to create and edit a to-do list with persistent stored in a Firebase backend.
+              <p class="card-text">A web app that allows users to create and edit a to-do list with persistent data stored in a Firebase back-end.
               <br></br>
               <br></br>
               <span id="project-bold-text">Tech stack:</span> JavaScript, HTML, CSS, Bootstrap, Firebase
               <br></br>
               </p>
-              <a href="https://todochecklistapp.netlify.app/" class="btn btn-primary project-button" id="visit-site-button">Visit Site</a>
+              <a href="https://todochecklistapp.netlify.app" class="btn btn-primary project-button" id="visit-site-button">Visit Site</a>
               <a href="https://github.com/ameyer12/to-do-app" class="btn btn-primary project-button">GitHub</a>
+            </div>
+          </div>
+          <div class="card" id="project-card">
+            <img class="card-img-top" src={informationalWebsiteImage} alt="Project"></img>
+            <div class="card-body">
+              <h5 class="card-title">Informational Website</h5>
+              <p class="card-text">My goal with this project was to further improve my design and UI optimization skills. All images were designed by me.
+              <br></br>
+              <br></br>
+              <span id="project-bold-text">Tech stack:</span> JavaScript, React, HTML, CSS, Bootstrap, Canva
+              <br></br>
+              </p>
+              <a href="https://losangelesmountains123.netlify.app/" class="btn btn-primary project-button" id="visit-site-button">Visit Site</a>
+              <a href="https://github.com/ameyer12/informational-website" class="btn btn-primary project-button">GitHub</a>
+            </div>
+          </div>
+          <div class="card" id="project-card">
+            <img class="card-img-top" src={underConstructionImage} alt="Project"></img>
+            <div class="card-body">
+              <h5 class="card-title">DFW Sports Scanner</h5>
+              <p id="under-construction-card-p" class="card-text">This app will help users identify upcoming DFW area professional sports games and locate the stadium/arena utilizing Google Maps.
+              <br></br>
+              <br></br>
+              {/* <span id="project-bold-text">Tech stack:</span> Undecided */}
+              <br></br>
+              </p>
+              <a 
+                href="/" class="btn btn-primary project-button" 
+                id="visit-site-button"
+                onClick={(ev) => {
+                  ev.preventDefault();
+                  swal({
+                    title: "Development in progress",
+                    text: "Check back later!",
+                    customClass: "swal-alert"
+                  });
+                }}
+              >
+              Visit Site</a>
+              <a 
+                href="/" 
+                class="btn btn-primary project-button"
+                onClick={(ev) => {
+                  ev.preventDefault();
+                  swal({
+                    title: "Development in progress",
+                    text: "Check back later!",
+                    customClass: "swal-alert"
+                  });
+                }}
+              >
+              GitHub</a>
             </div>
           </div>
           </div>     

acb3411cce60ed1845b838dee82dcb0c338efc3c
Austin <austinmeyer1106@gmail.com>
2023-03-28 21:09:13 -0500

    pushing my solution for the 03272023 challenge

---
+++ rendezvous-with-cassidoo/03282023-practice.js
@@ -0,0 +1,21 @@
+// Given a list of numbers, return all groups of repeating consecutive numbers.
+function generateRandomInt(max){
+    return Math.floor(Math.random() * max + 1);
+}
+
+function rollDice(string){
+    let total = 0;
+    let stringArray = string.split("+");
+
+    for(let i = 0; i < stringArray.length; i++){
+        let diceArray = stringArray[i].split("d");
+        let numberOfDiceToRoll = parseInt(diceArray[0]);
+
+        while(numberOfDiceToRoll > 0){
+            total += generateRandomInt(diceArray[1]);
+            numberOfDiceToRoll--;
+        }
+        numberOfDiceToRoll = 0;
+    }
+    return total;
+}

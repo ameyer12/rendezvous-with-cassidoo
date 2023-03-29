8339c9946d87a23247fa237899cccb285046a7a4
ameyer12 <105730686+ameyer12@users.noreply.github.com>
2023-03-28 21:10:34 -0500

    Delete Portfolio directory

--- Portfolio/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as Home } from "./Home";
-export { default as About } from "./About";
-export { default as Contact } from "./Contact";
-export { default as Sidebar } from "./Sidebar";
-export { default as RightSidebar } from "./RightSideBar";
-export { default as Projects} from "./Projects";
-export { default as MobileFooter} from "./MobileFooter";

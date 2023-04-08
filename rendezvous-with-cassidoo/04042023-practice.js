7836f4192c93db175a729a67ab69495fab217ff7
Austin <austinmeyer1106@gmail.com>
2023-04-08 00:13:30 -0500

    fixed a minor typo in the solution

--- rendezvous-with-cassidoo/04042023-practice.js
+++ rendezvous-with-cassidoo/04042023-practice.js
@@ -41,7 +41,7 @@ let suffixes = {
 
 function printCharacterAmount(string) {
     let result = [];
-    let joinedString = string.split(" ").join("");
+    let joinedString = string.split(" ").join(" ");
     let numberOfCharacters = joinedString.length.toString();
 
     if(joinedString.length < 10){
@@ -102,3 +102,6 @@ function printCharacterAmount(string) {
     }
     return "Number too large. Please enter a number less than 10,000.";
 }
+
+
+console.log(printCharacterAmount("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."))

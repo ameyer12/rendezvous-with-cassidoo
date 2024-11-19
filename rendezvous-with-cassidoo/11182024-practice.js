da5fd32d8e5f9fed7a5e989a5f2c302c1ad8f7e7
Austin <austinmeyer1106@gmail.com>
2024-11-18 18:05:12 -0600

    practice problem for 11/17

---
+++ rendezvous-with-cassidoo/11182024-practice.js
@@ -0,0 +1,24 @@
+// Function that determines the maximum profit you can achieve by buying and selling the stock once
+function maxTheStock(arr) {
+  // Variables to store the minimum buy price and maximum profit
+  let minBuyPrice = Infinity;
+  let maxProfit = 0;
+
+  // Loop through input array
+  for(let i = 0; i < arr.length; i++) {
+    // Variable to store the current price and profit
+    let price = arr[i];
+    let profit = arr[i] - minBuyPrice;
+
+    // If the price is less than the current minBuyPrice, update the minBuyPrice
+    minBuyPrice = Math.min(minBuyPrice, price);
+
+    // If the profit is more than the current maxProfit, update the maxProfit
+    maxProfit = Math.max(maxProfit, profit);
+  }
+  // Return the maxProfit
+  return maxProfit;
+}
+
+// console.log(maxTheStock([7, 1, 5, 3, 6, 4]));
+// console.log(maxTheStock([7, 6, 4, 3, 1]));

db4464f20ee6e42e8a0ccfed9ef7cd420c42ca64
Austin <austinmeyer1106@gmail.com>
2025-04-04 15:08:27 -0500

    practice question for 4/4/2025

--- rendezvous-with-cassidoo/04042025-practice.js
+++ rendezvous-with-cassidoo/04042025-practice.js
@@ -17,15 +17,15 @@ function findLongestTimeGap(arr) {
     // Pointer to check next number
     let j = i + 1; 
 
-    // Variables to store the number of hours for both numbers (int)
+    // Number of hours for both numbers (int)
     let time1Hrs = parseInt(arr[i].split(":")[0]);
     let time2Hrs = parseInt(arr[j].split(":")[0]);
 
-    // Variables to store the number of minutes for both numbers (int)
+    // Number of minutes for both numbers (int)
     let time1Mins = parseInt(arr[i].split(":")[1]);
     let time2Mins = parseInt(arr[j].split(":")[1]);
 
-    // Variables to store the total minutes for both numbers
+    // Total minutes for both numbers
     let totalTime1 = (time1Hrs * 60) + time1Mins;
     let totalTime2 = (time2Hrs * 60) + time2Mins;
 
@@ -38,8 +38,3 @@ function findLongestTimeGap(arr) {
 
   return largestTimeGap;
 }
-
-// console.log(findLongestTimeGap(['12:00']));
-// console.log(findLongestTimeGap(['09:00', '11:00']));
-// console.log(findLongestTimeGap(['14:00', '09:00', '15:00', '10:30']));
-// console.log(findLongestTimeGap(['08:00', '10:00', '10:00', '14:00']));
